bbc8c087f987948c565cbd6a120eb2deb546edfb
Manuel.Puerto <Manuel.Puerto@solera.com>
2022-11-04 09:26:35 +0100

    first commit

---
+++ trialProyect/src/main/java/com/example/trialProyect/TrialProyectApplication.java
@@ -0,0 +1,15 @@
+package com.example.trialProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialProyectApplication.class, args);
+	}
+
+}
+
+
---
+++ trialProyect/src/test/java/com/example/trialProyect/TrialProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trialProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
